aeb36054a68d10c8afbb28348fa3aeb4b69163b8
Suvodeep90 <31140098+Suvodeep90@users.noreply.github.com>
2018-08-31 13:29:50 -0400

    added code for accessing github and add the data to pickle file

--- buggy_commit.py
+++ buggy_commit.py
@@ -1,16 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 28 14:16:17 2018
+Created on Fri Aug 31 12:49:38 2018
 
 @author: suvod
 """
 
-import sys
-import pandas as pd
-import timeit
+import git2repo
+import pygit2
 import re
-import numpy as np
-import unicodedata
-import nltk.corpus
-import nltk.stem
-import sys
+import pandas as pd
+from datetime import datetime
+import re, unicodedata
+from pygit2 import GIT_SORT_TOPOLOGICAL, GIT_SORT_REVERSE
+import os
+from utils import utils
+
+class buggy_commit_maker(object):
+    
+    
+    def __init__(self,project_name):
+        self.project_name = project_name
+        self.data_path = os.getcwd() + '\\data\\'
+        self.commit = self.read_files('commit')
+        self.committed_files = self.read_files('committed_files')
+        
+        
+    def read_files(self,file_data):
+        file_path = self.data_path + self.project_name + '_' + file_data + '.pkl'
+        return pd.read_pickle(file_path)
+    
+    
+    def isBuggyCommit(commit):
+        res=re.search(r'\b{bug|defect|fix|patch|#}\b',utils().stemming(commit.message),re.IGNORECASE)
+        if res is not None:
+            return True
+
+    
--- git2repo.py
+++ git2repo.py
@@ -9,6 +9,7 @@ from pygit2 import clone_repository
 from pygit2 import GIT_SORT_TOPOLOGICAL, GIT_SORT_REVERSE,GIT_MERGE_ANALYSIS_UP_TO_DATE,GIT_MERGE_ANALYSIS_FASTFORWARD,GIT_MERGE_ANALYSIS_NORMAL,GIT_RESET_HARD
 from pygit2 import Repository
 import shutil,os
+import pygit2
 from os import listdir
 from os.path import isfile, join
 from datetime import datetime
@@ -21,8 +22,11 @@ class git2repo(object):
         self.repo_name = repo_name
         self.repo_path = os.getcwd() + '\\temp_repo\\' + repo_name
         
-        
     def clone_repo(self):
+        git_path = pygit2.discover_repository(self.repo_path)
+        if git_path is not None:
+            self.repo = pygit2.Repository(git_path)
+            return self.repo
         if not os.path.exists(self.repo_path):
             os.makedirs(self.repo_path)
         self.repo = clone_repository(self.repo_url, self.repo_path)
@@ -40,7 +44,7 @@ class git2repo(object):
             shutil.rmtree(self.repo_path,ignore_errors=True)
             
         
-    def get_commits(self):
+    def get_commit_objects(self):
         commits = []
         for commit in self.repo.walk(self.repo.head.target, GIT_SORT_TOPOLOGICAL | GIT_SORT_REVERSE):
             commits.append(commit)
@@ -48,24 +52,29 @@ class git2repo(object):
     
     def get_committed_files(self):
         committed_files = []
-        commits = self.get_commits()
-        for i in range(len(commits)-1):
-            t0 = commits[i]
-            t1 = commits[i+1]
-            _diff = self.repo.diff(t0,t1)
-            for j in _diff.deltas:
-                committed_files.append([commits[i].id,j.new_file.id, j.new_file.mode,j.new_file.path])
-        return committed_files
-    
-    def get_diffs(self,commits):
-        diffs = []
-        for i in range(len(commits)-1):
+        commits = self.get_commit_objects()
+        for i in range(len(commits)):
             t0 = commits[i]
             if i != 0:
                 t1 = commits[i].parents[0]
             else:
                 continue
-            _diff = self.repo.diff(t0,t1)
+            _diff = self.repo.diff(t1,t0)
+            for j in _diff.deltas:
+                committed_files.append([commits[i].id.hex,j.new_file.id.hex, j.new_file.mode,j.new_file.path])
+        return committed_files
+    
+    def get_diffs(self,commits):
+        diffs = {}
+        commits = self.repo.get(commits)
+        for i in range(len(commits)):
+            t0 = commits[i]
+            files = {}
+            if i != 0:
+                t1 = commits[i].parents[0]
+            else:
+                continue
+            _diff = self.repo.diff(t1,t0)
             for diff_i in _diff.__iter__():
                 file_path = diff_i.delta.new_file.path
                 old_lineno = []
@@ -74,9 +83,24 @@ class git2repo(object):
                     for y in x.lines:
                         old_lineno.append(y.old_lineno)
                         new_lineno.append(y.new_lineno)
-                diffs.append([commits[i].id,file_path,old_lineno,new_lineno])
+                files[diff_i.delta.new_file.id] = {'file_path':file_path, 'old_lines':old_lineno,'new_lines':new_lineno}
+            diffs[commits[i].id] = {'files':files,'object':commits[i]}
         return diffs
     
     def get_blame(self,file_path):
         return self.repo.blame(file_path,flags = 'GIT-BLAME_TRACK_COPIES_ANY_COMMIT_COPIES')
+    
+    
+    def get_commits(self):
+        _commits = self.get_commit_objects()
+        commits = []
+        for commit in _commits:
+            commit_id = commit.id.hex
+            commit_message = commit.message
+            if len(commit.parent_ids) == 0:
+                commit_parent = None
+            else:
+                commit_parent = commit.parent_ids[0].hex
+            commits.append([commit_id,commit_message,commit_parent])
+        return commits
         
